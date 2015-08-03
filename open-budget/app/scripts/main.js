b58e4cee10b1e7c30a186044909da57bd52e70b7
Yonatan <yonatan.naor@gmail.com>
2015-08-03 21:11:00 +0300

    remove generated file

--- open-budget/app/scripts/main.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  require(["modernizr", "hello", "templates", "searchbar", "training", "subscribe", "team", "main_page_vis", "budget_view", "entity_details", "models", "bootstrap-tour"], function() {});
-
-}).call(this);
-
-/*
-//@ sourceMappingURL=main.js.map
-*/
