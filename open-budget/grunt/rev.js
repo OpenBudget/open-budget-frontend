57632f720e4775d3c06b2404476fd485bbbc30c3
Bnaya Peretz <me@bnaya.net>
2015-11-05 15:06:18 +0200

    fix missing svgs

--- open-budget/grunt/rev.js
+++ open-budget/grunt/rev.js
@@ -4,7 +4,8 @@ module.exports = {
           src: [
             '<%= yeoman.dist %>/scripts/{,*/}*.js',
             '<%= yeoman.dist %>/styles/{,*/}*.css',
-            '<%= yeoman.dist %>/styles/assets/**/*.*'
+            '<%= yeoman.dist %>/styles/assets/**/*.*',
+            '!<%= yeoman.dist %>/styles/assets/**/*.svg'
           ]
         }
       }
