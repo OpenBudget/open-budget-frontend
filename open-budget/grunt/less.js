43790742028157738f751173338bd3bd7d775b7d
Bnaya Peretz <me@bnaya.net>
2015-11-30 22:38:29 +0200

    fix css less sourcemaps

--- open-budget/grunt/less.js
+++ open-budget/grunt/less.js
@@ -2,11 +2,22 @@ module.exports = {
   dist: {
     files: {
         '<%= yeoman.app %>/styles/main.css': ['<%= yeoman.app %>/styles/main.less'],
-        '<%= yeoman.app %>/styles/spinner.css': ['<%= yeoman.app %>/styles/spinner.less']
     },
     options: {
       sourceMap: true,
       sourceMapFilename: '<%= yeoman.app %>/styles/main.css.map',
+      sourceMapBasepath: '<%= yeoman.app %>/',
+      sourceMapRootpath: '/'
+    }
+  },
+
+  spinner: {
+    files: {
+        '<%= yeoman.app %>/styles/spinner.css': ['<%= yeoman.app %>/styles/spinner.less']
+    },
+    options: {
+      sourceMap: true,
+      sourceMapFilename: '<%= yeoman.app %>/styles/spinner.css.map',
       sourceMapBasepath: '<%= yeoman.app %>/',
       sourceMapRootpath: '/'
     }
