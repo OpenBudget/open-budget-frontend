e2f74fe16486da47b866c54b7ec3599ad3e0f26f
Bnaya Peretz <me@bnaya.net>
2016-08-27 02:58:54 +0300

    grunt cleanup

--- open-budget/grunt/webpack.js
+++
@@ -1,9 +0,0 @@
-var webpackConfig = require('../webpack.config.js');
-var webpack = require('webpack');
-
-// minify/uglify css/js
-webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({comments: false}));
-
-module.exports = {
-  main: webpackConfig
-};
